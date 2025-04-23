27fdae0db6df6ffd1e40e120086c8b1bd565e767
jmshgs <james@jxhug.me>
2025-04-23 14:22:06 -0700

    finished rodeo

--- src/lib/components/article-title.svelte
+++ src/lib/components/article-title.svelte
@@ -7,10 +7,10 @@
 
 <div class="flex w-full">
     <div class="w-1/2">
-        <h1 class="m-0 sticky inset-0 dark:bg-[#1e1e1e]">{artist} - {name} ({year})</h1>
+        <h1 class="m-0 sticky inset-0 bg-white dark:bg-[#1e1e1e]">{artist} - {name} ({year})</h1>
         <slot/>
     </div>
-    <div class="flex grow justify-center items-center sticky inset-0">
-        <img class="w-96 mt-5 p-0" alt="the album cover" src={"/src/routes" + image} />
+    <div class="flex grow justify-center sticky inset-0">
+        <img class="w-96 mt-5 p-0 h-96" alt="the album cover" src={"/src/routes" + image} />
     </div>
 </div>
